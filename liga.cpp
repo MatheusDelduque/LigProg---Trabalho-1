d6e4c9bccb583bac03df2fdf53185df78724e0e4
MatheusDelduque <matheusdelduque1@gmail.com>
2024-05-16 17:52:12 -0300

    method created to discover the highest goal difference per championship

--- liga.cpp
+++ liga.cpp
@@ -21,33 +21,53 @@ vector<Team> Liga::getTeams()
 
 void Liga::highestGoalDifferenceChampionship(vector<Team> teams)
 {
-    string teamHighestGoalDifference;
-    int highestGoalDifference = 0;
-    int goalDifference = 0;
-    for (unsigned int index = 0; index < teams.size(); index++)
+    // Vetor para armazenar a diferença de gols maior em cada campeonato
+    vector<string> highestGoalDifferenceList;
+
+    // Percorre cada campeonato
+    for (unsigned int championship = 0; championship < teams[0].getGoalsScored().size(); championship++)
     {
-        Team team = teams[index];
+        // Inicializa a diferença de gols maior do campeonato como 0
+        int highestGoalDifference = 0;
+        // Guarda o indice do time com a maior diferença de gols
+        int highestGoalDifferenceIndex = 0;
 
-        vector<vector<unsigned int>> goalsScored = team.getGoalsScored();
-        vector<vector<unsigned int>> goalsConceded = team.getGoalsConceded();
-
-        for (unsigned int championship = 0; championship < goalsScored.size(); championship++)
+        // Percorre cada time
+        for (unsigned int index = 0; index < teams.size(); index++)
         {
+            // Pega o time atual
+            Team team = teams[index];
 
+            // Inicializa a diferença de gols do time em 0
+            int goalDifference = 0;
 
-
-            for (unsigned int year = goalsScored[championship].size(); year > 0; year--)
+            // Percorre cada ano do campeonato
+            for (unsigned int year = 0; year < team.getGoalsScored()[championship].size(); year++)
             {
-                goalDifference += goalsScored[championship][year] - goalsConceded[championship][year];
-                if (goalDifference > highestGoalDifference)
-                {
-                    highestGoalDifference = goalDifference;
-                    teamHighestGoalDifference = team.getName();
-                }
+                // Soma a diferença de gols do time em um ano específico
+                goalDifference += team.getGoalsScored()[championship][year] - team.getGoalsConceded()[championship][year];
             }
-            cout << "O time com o maior saldo de gols no Campeonato " << championship << " eh: " << teamHighestGoalDifference << " com a diferenca de: " << highestGoalDifference << endl;
-            goalDifference = 0;
-            highestGoalDifference = 0;
+
+            // Se for o primeiro time, guarda a diferença de gols como a maior
+            if (index == 0)
+            {
+                highestGoalDifference = goalDifference;
+            }
+
+            // Se a diferença de gols do time atual for maior que a maior encontrada
+            if (goalDifference > highestGoalDifference)
+            {
+                // Guarda a diferença de gols do time atual como a maior encontrada
+                highestGoalDifference = goalDifference;
+                // Guarda o indice do time com a maior diferença de gols
+                highestGoalDifferenceIndex = index;
+            }
         }
+
+        // Adiciona a diferença de gols maior do campeonato ao vetor
+        highestGoalDifferenceList.push_back(to_string(highestGoalDifference));
+
+        // Exibe o time com a maior diferença de gols no campeonato
+        cout << "O " << teams[highestGoalDifferenceIndex].getName() << " no campeonato " << championship + 1 << " teve o maior saldo de gols de: " << highestGoalDifferenceList[championship] << endl;
     }
 }
