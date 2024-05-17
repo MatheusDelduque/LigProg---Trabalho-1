6b570b7dac96b30dee58ba9389619e654f8ad882
MatheusDelduque <matheusdelduque1@gmail.com>
2024-05-17 03:09:54 -0300

    Comentando o codigo

--- time.cpp
+++ time.cpp
@@ -7,6 +7,7 @@
 
 using namespace std;
 
+// Construtor da classe Team.
 Team::Team(string name, vector<vector<unsigned int>> goalsScored, vector<vector<unsigned int>> goalsConceded)
 {
     setName(name);
@@ -14,30 +15,36 @@ Team::Team(string name, vector<vector<unsigned int>> goalsScored, vector<vector<
     setGoalsConceded(goalsConceded);
 };
 
+// Define o nome do time.
 void Team::setName(string newName)
 {
     name = newName;
 };
 
+// Retorna os gols marcados de cada campeonato.
 vector<vector<unsigned int>> Team::getGoalsScored(){
     return goalsScored;
 }
 
+// Retorna os gols sofridos de cada campeonato.
 vector<vector<unsigned int>> Team::getGoalsConceded()
 {
     return goalsConceded;
 }
 
+// Define os gols marcados de cada campeonato.
 void Team::setGoalsScored(vector<vector<unsigned int>> newGoalsScored)
 {
     goalsScored = newGoalsScored;
 }
 
+// Define os gols sofridos de cada campeonato.
 void Team::setGoalsConceded(vector<vector<unsigned int>> newGoalsConceded)
 {
     goalsConceded = newGoalsConceded;
 }
 
+// Retorna o nome do time.
 string Team::getName(){
     return name;
 }
